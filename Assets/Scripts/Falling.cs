9d48a5aed9eb4ea5def541d209310889a9b5dad7
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2024-03-10 18:11:05 +0200

    something

--- Assets/Scripts/Falling.cs
+++ Assets/Scripts/Falling.cs
@@ -3,15 +3,9 @@ using UnityEngine;
 public class Falling : MonoBehaviour
 {
     public float speed = 5f;
-    public float stopY = -3.5f;
 
     void Update()
     {
         transform.Translate(Vector2.down * speed * Time.deltaTime);
-
-        if (transform.position.y <= stopY)
-        {
-            transform.position = new Vector2(transform.position.x, stopY);
-        }
     }
 }
