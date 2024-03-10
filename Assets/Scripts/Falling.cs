c91db2aeed28ea9c0a0d6d56d0a0065d50b3d940
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2024-03-10 17:20:38 +0200

    falling coordinates

--- Assets/Scripts/Falling.cs
+++ Assets/Scripts/Falling.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Falling : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float fallSpeed = 5f;
+    public float stopY = -3.5f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.down * fallSpeed * Time.deltaTime);
+
+        if (transform.position.y <= stopY)
+        {
+            transform.position = new Vector2(transform.position.x, stopY);
+        }
     }
 }
