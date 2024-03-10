91b71bf2039782ecb573451294f1793672ba3f15
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2024-03-10 17:39:55 +0200

    vfe

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,18 +1,35 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class FruitSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> fruitPrefabs;
+    public float spawnRate = 1f; 
+    public float spawnHeight = 6f;
+    public float spawnWidth = 8f;
+
+    private void Update()
     {
-        
+        SpawnFruit();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Click()
     {
-        
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float randomX = Random.Range(-spawnWidth / 2f, spawnWidth / 2f);
+        Vector2 spawnPosition = new Vector2(randomX, mousePosition.y);
+
+        for (int i = 0; i < fruitPrefabs.Count; i++)
+        {
+            foreach (var fruit in fruitPrefabs)
+            {
+                Instantiate(fruit, spawnPosition, Quaternion.identity);
+            }
+        }
+    }
+
+    void SpawnFruit()
+    {
+        Click();        
     }
 }
