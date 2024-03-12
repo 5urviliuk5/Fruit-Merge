b390eabaa17989750226759a669763a7de59fdb7
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2024-03-12 21:25:54 +0200

    spawn finished

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class FruitSpawner : MonoBehaviour
 {
     public List<GameObject> fruitPrefabs;
+    public float spawnDelay = 2f;
+    private float nextSpawnTime;
+
+    void Start()
+    {
+        nextSpawnTime = Time.time;
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && Time.time >= nextSpawnTime)
         {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            GameObject selectedFruit = fruitPrefabs[Random.Range(0, fruitPrefabs.Count)];
-            Vector2 spawnPosition = new Vector2(mousePosition.x, 4f);
-            Instantiate(selectedFruit, spawnPosition, Quaternion.identity);
+            SpawnFruit();
         }
+    }
+
+    void SpawnFruit()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GameObject selectedFruit = fruitPrefabs[Random.Range(0, fruitPrefabs.Count)];
+        Vector2 spawnPosition = new Vector2(mousePosition.x, 4f);
+        Instantiate(selectedFruit, spawnPosition, Quaternion.identity);
+        nextSpawnTime = Time.time + spawnDelay;
     }
 }
