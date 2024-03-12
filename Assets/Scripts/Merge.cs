fcb9146f553c675e0039ac1f996c32fab9ef82ac
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2024-03-12 20:43:25 +0200

    merge

--- Assets/Scripts/Merge.cs
+++ Assets/Scripts/Merge.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.Collections.AllocatorManager;
 
 public class Merge : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int ID;
+    public GameObject mergedFruit;
+
     void Start()
+    {
+        ID = GetInstanceID();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        if (other.gameObject.GetComponent<SpriteRenderer>().color == GetComponent<SpriteRenderer>().color)
+        {
+            if (ID < other.gameObject.GetComponent<Merge>().ID) return;
+            GameObject O = Instantiate(mergedFruit, transform.position, Quaternion.identity) as GameObject;
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
